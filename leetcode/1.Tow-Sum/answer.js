0e3ef8d2ed02de82e406023620c3722b6c799ff7
muzhao.wc <muzhao.wc@alibaba-inc.com>
2021-03-29 15:35:52 +0800

    finish question 1

---
+++ leetcode/1.Tow-Sum/answer.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  // 动态哈希表法
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    const num1 = nums[i];
+    const num2 = target - nums[i];
+    if (map.has(num2)) {
+      return [map.get(num2), i];
+    } else {
+      map.set(num1, i);
+    }
+  }
+};
