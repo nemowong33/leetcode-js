6433e4019716429cdababf467b5605c3eba4715b
muzhao.wc <muzhao.wc@alibaba-inc.com>
2021-03-30 15:39:16 +0800

    Complete the third question

--- src/leetcode/0003.Longest-Substring-Without-Repeating-Characters/answer.js
+++ src/leetcode/0003.Longest-Substring-Without-Repeating-Characters/answer.js
@@ -3,9 +3,27 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  let map = new Map();
-  const strAry = s.split('');
-  for (let i = 0; i < strAry.length; i++) {
+  var len = s.length,
+    max = 0,
+    chars = new Set(),
+    leftBound = 0,
+    ch,
+    i;
 
+  for (i = 0; i < len; i++) {
+    ch = s.charAt(i);
+
+    if (chars.has(ch)) {
+      while (leftBound < i && s.charAt(leftBound) !== ch) {
+        chars.delete(s.charAt(leftBound));
+        leftBound++;
+      }
+      leftBound++;
+    } else {
+      chars.add(ch);
+      max = Math.max(max, i - leftBound + 1);
+    }
   }
+
+  return max;
 };
